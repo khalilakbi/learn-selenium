2633c59162b538b3539cd45bea9b97cc84656530
gayaaris <rachidaifri90@gmail.com>
2024-10-10 17:30:52 -0400

    firsthomework

--- src/test/java/rachida/HomeWork1.java
+++ src/test/java/rachida/HomeWork1.java
@@ -1,4 +1,22 @@
 package rachida;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomeWork1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");
+        String d = driver.getTitle();
+        if(d.equals("Your Store")){
+            System.out.println("pass");
+
+        } else {
+            System.out.println("failed");
+
+        }
+
+    }
 }
